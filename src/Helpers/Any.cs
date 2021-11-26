d78e9d3408d126f7158bfcce465fdd8bd4cb5519
Alexander Liakhovets <alexanderliakhovets@gmail.com>
2021-11-26 23:54:04 +0300

    Add Any.cs class with extensions  and test for them

---
+++ src/Helpers/Any.cs
@@ -0,0 +1,91 @@
+﻿namespace Helpers;
+
+public static class Any
+{
+    private static int _min;
+    private static int _max;
+    private const int MaxAttemptsNumber = 1000;
+
+    public const int IntMaxValue = 100000;
+    public const int IntMinValue = -100000;
+
+    public static string String(int length = 10)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        var random = new Random();
+        var randomString = new string(Enumerable
+            .Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)])
+            .ToArray());
+
+        return randomString;
+    }
+
+    public static int Int(int min = IntMinValue, int max = IntMaxValue)
+    {
+        _min = min;
+        _max = max;
+
+        return new Random().Next(min, max);
+    }
+
+    public static int Except(this int obj, params int[] items)
+    {
+        var attemptNumber = 0;
+        var enumerable = items.ToList();
+
+        while (enumerable.Contains(obj))
+        {
+            if (attemptNumber++ > MaxAttemptsNumber)
+            {
+                throw new ArgumentOutOfRangeException($"It is too complex to generate unique-random value for: ${obj}");
+            }
+
+            obj = Int(_min, _max);
+        }
+
+        return obj;
+    }
+
+    public static int PositiveInt(int max = 100000)
+    {
+        return Int(0, max);
+    }
+
+    public static int NegativeInt(int min = -100000)
+    {
+        return Int(min, 0);
+    }
+
+    public static Guid Guid()
+    {
+        return System.Guid.NewGuid();
+    }
+
+    public static string Except(this string obj, params string[] items)
+    {
+        var attemptNumber = 0;
+        var enumerable = items.ToList();
+
+        while (enumerable.Contains(obj))
+        {
+            if (attemptNumber++ > MaxAttemptsNumber)
+            {
+                throw new ArgumentOutOfRangeException($"It is too complex to generate unique-random value for: ${obj}");
+            }
+
+            obj = String(obj.Length);
+        }
+
+        return obj;
+    }
+
+    public static T From<T>(this T obj, params T[] items)
+    {
+        Random rand = new();
+
+        var enumerable = items.ToList();
+        return enumerable[rand.Next(enumerable.Count - 1)];
+    }
+}
---
+++ test/UnitTests/AnyTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Helpers;
+using Xunit;
+
+namespace UnitTests;
+
+public class AnyTests
+{
+    [Fact]
+    public void Generate_random_fixed_length_string()
+    {
+        var length = new Random().Next(maxValue: 100);
+
+        var rndString = Any.String(length);
+
+        rndString.Length.Should().Be(length);
+    }
+
+    [Fact]
+    public void Generate_random_string_except_provided()
+    {
+        var exclusions = new[] { "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        var rndString = Enumerable.Range(0, 100)
+            .Select(_ => Any.String(1).From(exclusions));
+
+        exclusions.Should().NotBeSubsetOf(rndString);
+    }
+
+    [Fact]
+    public void Generate_string_only_from_provided_list()
+    {
+        var source = new[] { "F", "G", "H", "I", "J", "K", "L", "M", "N" };
+
+        var generatedStrings = Enumerable.Range(0, 100)
+            .Select(_ => Any.String(1).From(source));
+
+        generatedStrings.Should().BeSubsetOf(source);
+    }
+
+    [Fact]
+    public void Generate_random_int()
+    {
+        var rndIntegers = Enumerable.Range(0, 100).Select(_ => Any.Int());
+
+        rndIntegers.Should().OnlyContain(i => i >= Any.IntMinValue && i <= Any.IntMaxValue);
+    }
+
+    [Fact]
+    public void Generate_random_int_except_provided()
+    {
+        var exclusions = new[] { 1, 2, 3, 4, 5, 6, 7 };
+        var rndIntegers = Enumerable.Range(0, 100)
+            .Select(_ => Any.Int(0, 10).Except(exclusions));
+
+        rndIntegers.Should().OnlyContain(i => !exclusions.Contains(i));
+    }
+
+    [Fact]
+    public void Generate_random_int_only_from_provided_list()
+    {
+        var source = new[] { 1, 2, 3 };
+        var rndIntegers = Enumerable.Range(0, 100)
+            .Select(_ => Any.Int().From(source));
+
+        rndIntegers.Should().OnlyContain(i => source.Contains(i));
+    }
+
+    [Fact]
+    public void Generate_random_int_greater_than_zero()
+    {
+        var rndIntegers = Enumerable.Range(0, 100)
+            .Select(_ => Any.PositiveInt());
+
+        rndIntegers.Should().OnlyContain(i => i > 0);
+    }
+
+    [Fact]
+    public void Generate_random_int_less_than_zero()
+    {
+        var rndIntegers = Enumerable.Range(0, 100)
+            .Select(_ => Any.NegativeInt());
+
+        rndIntegers.Should().OnlyContain(i => i < 0);
+    }
+
+    [Fact]
+    public void Generate_random_guid()
+    {
+        var guids = Enumerable.Range(0, 100)
+            .Select(_ => Any.Guid());
+
+        guids.Should().OnlyHaveUniqueItems();
+    }
+}
